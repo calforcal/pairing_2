d6bed6fb9f2b1a76f099d4cce8c1369370ca64ab
Michael Callahan <mcalla242@gmail.com>
2023-02-23 15:08:55 -0700

    Add if statements for my friends

--- friends.rb
+++ friends.rb
@@ -1,6 +1,9 @@
-best_friend = "Shrek"
-friend = "Donkey"
+best_friend = "Fiona"
+friend = "Gingerbread Man"
+good_friend = "Lord Farquaad"
 
 if best_friend == "Shrek" && friend == "Donkey"
     puts "#{best_friend} is my BEST friend and #{friend} is my other friend."
+elsif best_friend != "Shrek" || friend != "Donkey"
+    puts "#{good_friend} is a very good friend."
 end
